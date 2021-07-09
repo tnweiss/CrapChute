f667554276290a3399a619cb86494b54e7d0cd1d
Tyler Weiss <tnweiss5@gmail.com>
2021-07-09 15:21:56 -0700

    Initial Commit

---
+++ src/main/java/crapchute/crapchute/CrapchuteApplication.java
@@ -0,0 +1,13 @@
+package crapchute.crapchute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrapchuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrapchuteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crapchute/crapchute/CrapchuteApplicationTests.java
@@ -0,0 +1,13 @@
+package crapchute.crapchute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrapchuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
